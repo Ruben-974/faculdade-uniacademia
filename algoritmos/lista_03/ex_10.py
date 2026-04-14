0db99a06477c95ef84c39a3ffe1d166bf25e095f
Ruben-974 <87450219+Ruben-974@users.noreply.github.com>
2026-04-13 23:43:08 -0300

    update

--- algoritmos/lista_03/ex_10.py
+++ algoritmos/lista_03/ex_10.py
@@ -1,5 +1,11 @@
 # Escrever um algoritmo que leia um número n que indica quantos valores devem ser lidos a seguir. Para cada número lido, mostre o valor lido e o fatorial deste valor
 
+times = int(input('How many numbers can you read? '))
 
 
-
+for i in range(times):
+    number = int(input('Enter a number: '))
+    factorial = 1
+    for i in range(1, number+1):
+        factorial = factorial*i
+    print(f'{number}! = {factorial}')

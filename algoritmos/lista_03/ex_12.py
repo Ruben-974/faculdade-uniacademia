def56cc85f22fa65673edd20c194d3564b9271eb
alunouni <rubenisrael974@gmail.com>
2026-04-15 19:12:48 -0300

    update test

--- algoritmos/lista_03/ex_12.py
+++ algoritmos/lista_03/ex_12.py
@@ -1,3 +1,4 @@
 # Escrever um algoritmo que leia um número não determinado de valores e calcule a média aritmética dos valores lidos, a quantidade de valores positivos, a quantidade de valores negativos e o percentual de valores negativos e positivos. Mostre os resultados.
 
 print('teste')
+print('teste2')

c421c6a105934ae7b71eb36455da2c650d0107bf
alunouni <rubenisrael974@gmail.com>
2026-04-15 20:22:03 -0300

    update

---
+++ algoritmos/lista_03/ex_16.py
@@ -0,0 +1,16 @@
+# Escreva um algoritmo que lê um valor n inteiro e positivo e que calcula a
+# seguinte soma:
+# S = 1 + 1/2 + 1/3 + 1/4 + ... + 1/n
+# O algoritmo deve escrever cada termo gerado e o valor final de S.
+
+number = int(input('Enter a positive number: '))
+
+if number > 0:
+
+    for i in range(number+1):
+        print(f'1/{i+1}', end=' + ')
+        if i == number:
+            print(f'1/{i + 1}')
+
+else:
+    print('Value invalid!')

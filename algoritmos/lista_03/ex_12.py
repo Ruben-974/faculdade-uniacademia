42a3811deaf2211f86ff32aaa8edd1adcaa4d241
alunouni <rubenisrael974@gmail.com>
2026-04-15 19:30:07 -0300

    update test

--- algoritmos/lista_03/ex_12.py
+++ algoritmos/lista_03/ex_12.py
@@ -1,4 +1,29 @@
-# Escrever um algoritmo que leia um número não determinado de valores e calcule a média aritmética dos valores lidos, a quantidade de valores positivos, a quantidade de valores negativos e o percentual de valores negativos e positivos. Mostre os resultados.
+# Escrever um algoritmo que leia uma quantidade desconhecida de números
+# e conte quantos deles estão nos seguintes intervalos: [0,25], [26,50],
+# [51,75] e [76,100]. A entrada de dados deve terminar quando for lido um
+# número negativo.
 
-print('teste')
-print('teste2')
+number = 0
+interval_00_25 = 0
+interval_26_50 = 0
+interval_51_75 = 0
+interval_76_100 = 0
+
+while number >= 0:
+    number = int(input('Enter a number: '))
+    if number >= 0 and number <= 25:
+        interval_00_25 = interval_00_25 + 1
+    else:
+        if number >= 26 and number <= 50:
+            interval_26_50 = interval_26_50 + 1
+        else:
+            if number >= 51 and number <= 75:
+                interval_51_75 = interval_51_75 + 1
+            else:
+                if number >= 76 and number <= 100:
+                    interval_76_100 = interval_76_100 + 1
+
+print(f'Number of values between 00 and 025: {interval_00_25}')
+print(f'Number of values between 26 and 050: {interval_26_50}')
+print(f'Number of values between 51 and 075: {interval_51_75}')
+print(f'Number of values between 76 and 100: {interval_76_100}')

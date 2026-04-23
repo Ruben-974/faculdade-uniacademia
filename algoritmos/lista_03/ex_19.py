c2b52c1b0af109ffb7be5c60c59ad0fb99fe3b13
Ruben-974 <87450219+Ruben-974@users.noreply.github.com>
2026-04-23 08:39:03 -0300

    up ex19

--- algoritmos/lista_03/ex_19.py
+++ algoritmos/lista_03/ex_19.py
@@ -1,13 +1,18 @@
 # Escrever um algoritmo que leia um valor N inteiro e positivo e que calcula o valor de E. Imprime o resultado de E ao final.  
 # E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / N!
 
-
+result_E = 0
 number = int(input('Enter a number: '))
 
 if number >= 0:
-
-    for i in range(1, number):
+    print('1 + ', end='')
+    for i in range(1, number+1):
         factorial = 1
-        print(f'{1} + {1} / {i}!', end=' + ')
+        if i != number:
+            print(f'{1} / {i}!', end=' + ')
+        else:
+            print(f'{1} / {i}!', end='')
         for i in range(1, i):
             factorial *= i
+        result_E += 1/factorial
+print(f' = {result_E:.2f}')

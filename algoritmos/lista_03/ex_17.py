bf18e7cf8fabdd077c626fe86d0234c310652add
Rúben Israel <rubenisrael974@gmail.com>
2026-04-23 10:30:34 -0300

    .

--- algoritmos/lista_03/ex_17.py
+++ algoritmos/lista_03/ex_17.py
@@ -1,14 +1,14 @@
 # Escrever um algoritmo que lê 10 valores, um de cada vez, e conte quantos deles estão no intervalo [10,20] e quantos deles estão fora do intervalo, escrevendo estas informações.
 
-counter10_20 = 0
-counter = 0
+interval_counter = 0
+not_interval_counter = 0
 
 for i in range(10):
     number = int(input('Enter a number: '))
-    if number >= 10 or number <= 20:
-        counter10_20 += 1
+    if number >= 10 and number <= 20:
+        interval_counter += 1
     else:
-        counter += 1
+        not_interval_counter += 1
 
-print(counter)
-print(counter10_20)
+print(interval_counter)
+print(not_interval_counter)
--- algoritmos/lista_03/ex_18.py
+++ algoritmos/lista_03/ex_18.py
@@ -1,2 +1,17 @@
-# Escrever   um  algoritmo   que   gere   e   escreva   os   3   primeiros   números perfeitos. Um número perfeito é aquele que é igual a soma dos seus divisores. (Ex.: 6 = 1+2+3; 28= 1+2+4+7+14, etc).
+# Escrever   um  algoritmo   que   gere   e   escreva   os   3   primeiros  números perfeitos. Um número perfeito é aquele que é igual a soma dos seus divisores. (Ex.: 6 = 1+2+3; 28= 1+2+4+7+14, etc). 
 
+perfects = 0
+number = 0
+
+while perfects <= 3:
+    number += 1
+    sum_numbers = 0
+
+    for i in range(1, number):
+        if number%i==0:
+            sum_numbers += i
+
+    if sum_numbers == number:
+        print(f'{number} is number perfect!')
+        perfects += 1
+    

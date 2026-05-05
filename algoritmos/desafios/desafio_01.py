8112657791763ee521b3b53fbbe501aec4e1db32
Ruben-974 <87450219+Ruben-974@users.noreply.github.com>
2026-05-04 22:21:36 -0300

    forca

--- algoritmos/desafios/desafio_01.py
+++ algoritmos/desafios/desafio_01.py
@@ -7,28 +7,49 @@ print('/-------/     Papel   [1]    /--------/')
 print('/-------/     Tesoura [2]    /--------/')
 print()
 
-escolha_usuario = int(input('--------> Sua escolha: '))
-escolha_maquina = int(randint(0, 2))
+vitorias_maquina = 0
+vitorias_usuario = 0
 
-print()
+partidas = int(input('Quantas patidas você irá jogar? '))
 
-if escolha_usuario > 2 or escolha_usuario < 0:
+for c in range(partidas):
 
-    print('\033[31mEscolha invalida!')
+    escolha_usuario = int(input('--------> Sua escolha: '))
+    escolha_maquina = int(randint(0, 2))
 
-else:
-
-    print('------> Você escolheu:', escolha_usuario)
-    print('------> O PC escolheu:', escolha_maquina)
     print()
 
-    if escolha_usuario == escolha_maquina:
-        print('\033[34mEMPATE!')
+    if escolha_usuario > 2 or escolha_usuario < 0:
+
+        print('\033[31mEscolha invalida!\033[0m')
+
     else:
-        if (escolha_usuario == 0 and escolha_maquina == 1) or (escolha_usuario == 1 and escolha_maquina == 2) or (escolha_usuario == 2 and escolha_maquina == 0):
-            print('\033[31mO PC GANHOU!')
+
+        print('------> Você escolheu:', escolha_usuario)
+        print('------> O PC escolheu:', escolha_maquina)
+        print()
+
+        if escolha_usuario == escolha_maquina:
+            print('\033[34mEMPATE!\033[0m')
         else:
-            print('\033[32mVOCÊ GANHOU!')
+            if (escolha_usuario == 0 and escolha_maquina == 1) or (escolha_usuario == 1 and escolha_maquina == 2) or (escolha_usuario == 2 and escolha_maquina == 0):
+                print('\033[31mO PC GANHOU!\033[0m')
+                vitorias_maquina = vitorias_maquina + 1
+            else:
+                print('\033[32mVOCÊ GANHOU!\033[0m')
+                vitorias_usuario = vitorias_usuario + 1
 
 print()
-print('\033[0m/-------/  GAME FINALIZADO  /--------/')
+print('\033[0m/------/  GAME FINALIZADO  /--------/')
+print(f'/-----/ O usuario venceu {vitorias_usuario} /-----/')
+print(f'/-----/ A maquina venceu {vitorias_maquina} /-----/')
+print(f'/-----/ Houve no total {partidas-vitorias_maquina-vitorias_usuario} empates/-----/\033[0m')
+print()
+
+if vitorias_usuario > vitorias_maquina:
+    print('\033[0m/-----/ MAIOR VENCEDOR: USUARIO /------/')
+else:
+    if vitorias_usuario < vitorias_maquina:
+        print('\033[0m/-----/ MAIOR VENCEDOR: MAQUINA /------/')
+    else:
+        print('\033[0m/-----/ HOUVE EMPATE /-------/')
--- algoritmos/desafios/desafio_02.py
+++ algoritmos/desafios/desafio_02.py
@@ -1,55 +1,24 @@
-from random import *
+# JOGO DA FORCA
 
-print('/-------/    GAME JOKENPÔ    /--------/')
-print('/-------/ Escolha uma opção: /--------/')
-print('/-------/     Pedra   [0]    /--------/')
-print('/-------/     Papel   [1]    /--------/')
-print('/-------/     Tesoura [2]    /--------/')
-print()
+palavra = 'ALGORITMOS'
+letras_jogadas = ''
+chances = 6
+vitoria = False
 
-vitorias_maquina = 0
-vitorias_usuario = 0
+while chances != 0 or vitoria:
 
-partidas = int(input('Quantas patidas você irá jogar? '))
+    letra = input('\nDigite uma letra: ')
 
-for c in range(partidas):
+    for i in palavra:
+        
+        if i not in letras_jogadas:
+            letras_jogadas += letra
+            if not i in palavra:
+                chances -= 1
 
-    escolha_usuario = int(input('--------> Sua escolha: '))
-    escolha_maquina = int(randint(0, 2))
-
-    print()
-
-    if escolha_usuario > 2 or escolha_usuario < 0:
-
-        print('\033[31mEscolha invalida!\033[0m')
-
-    else:
-
-        print('------> Você escolheu:', escolha_usuario)
-        print('------> O PC escolheu:', escolha_maquina)
-        print()
-
-        if escolha_usuario == escolha_maquina:
-            print('\033[34mEMPATE!\033[0m')
+        
+        if i in letras_jogadas:
+            print(f'{i}', end=' ')
         else:
-            if (escolha_usuario == 0 and escolha_maquina == 1) or (escolha_usuario == 1 and escolha_maquina == 2) or (escolha_usuario == 2 and escolha_maquina == 0):
-                print('\033[31mO PC GANHOU!\033[0m')
-                vitorias_maquina = vitorias_maquina + 1
-            else:
-                print('\033[32mVOCÊ GANHOU!\033[0m')
-                vitorias_usuario = vitorias_usuario + 1
+            print('_', end=' ')
 
-print()
-print('\033[0m/------/  GAME FINALIZADO  /--------/')
-print(f'/-----/ O usuario venceu {vitorias_usuario} /-----/')
-print(f'/-----/ A maquina venceu {vitorias_maquina} /-----/')
-print(f'/-----/ Houve no total {partidas-vitorias_maquina-vitorias_usuario} empates/-----/\033[0m')
-print()
-
-if vitorias_usuario > vitorias_maquina:
-    print('\033[0m/-----/ MAIOR VENCEDOR: USUARIO /------/')
-else:
-    if vitorias_usuario < vitorias_maquina:
-        print('\033[0m/-----/ MAIOR VENCEDOR: MAQUINA /------/')
-    else:
-        print('\033[0m/-----/ HOUVE EMPATE /-------/')

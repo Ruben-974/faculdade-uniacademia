a25c497adea399101e5f8237d4677623e7a3526c
Ruben-974 <87450219+Ruben-974@users.noreply.github.com>
2026-05-12 19:07:52 -0300

    up invensivel

--- algoritmos/desafios/desafio_02.1.py
+++ algoritmos/desafios/desafio_02.1.py
@@ -4,26 +4,27 @@
 
 from random import choice
 
-
 palavras = [
-    "abada", "abafa", "abala", "abana", "abata", "abava", "acaba", "achar", "acham", "achas",
-    "acama", "acana", "acara", "afaga", "afala", "afama", "afana", "afara", "afata", "afava",
-    "agata", "alada", "alaga", "alama", "alana", "alara", "alava", "amada", "amala", "amana",
-    "amara", "amata", "amava", "anana", "apaga", "apara", "arara", "arava", "aspas", "atada",
-    "atala", "atara", "atava", "avara", "babam", "babar", "babas", "bagas", "bamba", "banal",
-    "barba", "barca", "barra", "basta", "batas", "braca", "brama", "cabra", "cacar", "cacas",
-    "cacha", "calca", "calma", "camba", "cansa", "canta", "capar", "capas", "carta", "casca",
-    "caspa", "catar", "cavar", "chaga", "chama", "chata", "clava", "crava", "dadas", "danar",
-    "datar", "davam", "falar", "falas", "famas", "farpa", "farsa", "farta", "favas", "galas",
-    "gamar", "garra", "grama", "jarra", "lacra", "lagar", "lamas", "lanar", "lasca", "lavar",
-    "mamar", "manca", "manda", "manga", "manha", "manta", "marca", "massa", "matar", "nadar",
-    "nanar", "narra", "natal", "pagar", "panda", "parar", "parta", "pasta", "patas", "praca",
-    "prata", "ralar", "rampa", "rapaz", "raspa", "rasta", "ratas", "sacra", "salva", "samba",
-    "sanar", "sarda", "sarja", "sarna", "tarja", "trama", "trava", "valva", "vazar", "zanga",
-    "zarpar", "halar", "lacar", "lazar", "macar", "manar", "nasal", "rarar", "rasar", "salar",
-    "sarar", "tacar", "talar", "tapar", "tarar", "tasar", "vacar", "vagar", "valar", "varar",
-    "xaras", "beber", "bebes", "bebem", "ceder", "cedes", "cedem", "cerne", "deter", "dever",
-    "elege", "enche", "entre", "entes", "estes", "fedem", "feder", "fedes", "ferem", "feres",
+    "achar", "acham", "achas", "aspas",
+    "babam", "babar", "babas", "bagas", "bamba", "banal", "barba", "barca", "barra", "basta", "batas", "braca", "brama",
+    "cabra", "cacar", "cacas", "cacha", "calca", "calma", "camba", "cansa", "canta", "capar", "capas", "carta", "casca",
+    "caspa", "catar", "cavar", "chaga", "chama", "chata", "clava", "crava",
+    "dadas", "danar", "datar", "davam",
+    "falar", "falas", "famas", "farpa", "farsa", "farta", "favas",
+    "galas", "gamar", "garra", "grama",
+    "jarra",
+    "lacra", "lagar", "lamas", "lanar", "lasca", "lavar",
+    "mamar", "manca", "manda", "manga", "manha", "manta", "marca", "massa", "matar",
+    "nadar", "nanar", "narra", "natal",
+    "pagar", "panda", "parar", "parta", "pasta", "patas", "praca", "prata",
+    "ralar", "rampa", "rapaz", "raspa", "rasta", "ratas",
+    "sacra", "salva", "samba", "sanar", "sarda", "sarja", "sarna",
+    "tarja", "trama", "trava",
+    "valva", "vazar", "zanga",
+    "halar", "lacar", "lazar", "macar", "manar", "nasal", "rasar", "salar", "sarar",
+    "tacar", "talar", "tapar", "tarar", "tasar", "vacar", "vagar", "valar", "varar", "xaras",
+    "beber", "bebes", "bebem", "ceder", "cedes", "cedem", "cerne", "deter", "dever",
+    "enche", "entre", "entes", "estes", "fedem", "feder", "fedes", "ferem", "feres",
     "fezes", "frege", "geres", "gerem", "ledes", "lerem", "leres", "medem", "medes", "meter",
     "metes", "metem", "mexem", "mexer", "mexes", "nenem", "perde", "prece", "preme", "reger",
     "reges", "regem", "reles", "remem", "remes", "rente", "reses", "retem", "retes", "rever",
@@ -31,47 +32,43 @@ palavras = [
     "teces", "temem", "temer", "temes", "tende", "tenre", "terem", "teres", "teste", "tezes",
     "vezes", "vedes", "velem", "veles", "vende", "verde", "verse", "veste", "vetem", "vetes",
     "vexem", "vexes", "zeles", "zeres", "zerem", "neves", "lenes", "penes", "redes", "leves",
-    "celes", "meses", "leses", "peses", "civil", "civis", "fingi", "iris", "iriri", "mirim",
-    "minis", "miris", "siris", "vizir", "vivis", "fitis", "bitis", "kiwis", "litis", "mitis",
-    "pitis", "vitis", "xiris", "finis", "vinil", "bobos", "bolos", "botos", "cocos", "colos",
-    "comos", "coros", "docos", "donos", "foros", "fosos", "gomos", "goros", "jogos", "lotos",
-    "mocos", "modos", "molos", "monos", "moros", "notos", "novos", "ogros", "olhos", "omos",
-    "orcos", "osco", "ozono", "pocos", "polos", "pondo", "poros", "posso", "potro", "povos",
-    "psoco", "robos", "rolos", "romos", "rosos", "rotos", "socos", "soros", "tocos", "tolos",
-    "tomos", "tonos", "topos", "toros", "votos", "xoxos", "mofos", "bofos", "coxos", "fofos",
-    "hortos", "mortos", "ortos", "portos", "rostos", "tortos", "fosso", "ossos", "cosso",
-    "rosso", "urubu", "urucu", "gurus", "lupus", "mucum", "tucum", "fucus", "humus", "jugum",
+    "celes", "meses", "leses", "peses",
+    "civil", "civis", "fingi", "mirim", "minis", "miris", "siris", "vizir", "vivis",
+    "fitis", "bitis", "kiwis", "litis", "mitis", "pitis", "vitis", "xiris", "finis", "vinil",
+    "bobos", "bolos", "botos", "cocos", "colos", "comos", "coros", "docos", "donos", "foros",
+    "fosos", "gomos", "goros", "jogos", "lotos", "mocos", "modos", "molos", "monos", "moros",
+    "notos", "novos", "ogros", "olhos", "omos", "orcos", "pocos", "polos", "pondo",
+    "poros", "posso", "potro", "povos", "psoco", "robos", "rolos", "romos", "rosos", "rotos",
+    "socos", "soros", "tocos", "tolos", "tomos", "tonos", "topos", "toros", "votos", "xoxos",
+    "mofos", "bofos", "coxos", "fofos", "ortos", "fosso", "cosso", "rosso",
+    "gurus", "lupus", "mucum", "tucum", "fucus", "humus", "jugum",
     "causa", "pausa", "sauna", "fauna", "cauda", "lauda", "paula", "maula", "jaula", "sauva",
     "chuva", "chupa", "chuta", "chufa", "chula", "mudar", "mudas", "mudam", "durar", "duras",
     "duram", "furar", "furas", "furam", "jurar", "juras", "juram", "curar", "curas", "curam",
     "lugar", "sugar", "fugaz", "pulga", "culpa", "burra", "surra", "turba", "turva", "curva",
     "furta", "junta", "justa", "lutar", "mutar", "nutar", "bugar", "jugar", "mular", "fugir",
-    "fugis", "fugiu", "sumir", "sumis", "sumiu", "subir", "subis", "subiu", "curtir", "curtis",
-    "curtiu", "urgir", "urgis", "urgiu", "uniu", "punir", "punis", "puniu", "munir", "munis",
-    "muniu", "zunir", "zunis", "zuniu", "funil", "futil", "sutil", "fuzil", "rubim", "nubil",
-    "urdir", "urdis", "urdiu", "ruins", "viver", "vives", "vivem", "vivei", "ferir", "feris",
-    "gerir", "geris", "pedir", "pedis", "medir", "medis", "doido", "coiso", "noivo", "poiso",
-    "roido", "moido", "doudo", "couto", "mouro", "ouros", "pouco", "rouco", "bolou", "colou",
-    "mouco", "copas", "colar", "molar", "polar", "solar", "rolar", "topar", "boiar", "noiar",
-    "moiar", "goiar", "voar", "doar", "roer", "soer", "moer", "loer", "poer", "mover", "covil",
+    "fugis", "fugiu", "sumir", "sumis", "sumiu", "subir", "subis", "subiu", "urgir", "urgis", 
+    "urgiu", "punir", "punis", "puniu", "munir", "munis", "muniu", "zunir", "zunis", "zuniu", 
+    "funil", "futil", "sutil", "fuzil", "rubim", "nubil", "urdir", "urdis", "urdiu", "ruins", 
+    "viver", "vives", "vivem", "vivei", "ferir", "feris", "gerir", "geris", "pedir", "pedis", 
+    "medir", "medis", "doido", "coiso", "noivo", "poiso", "roido", "moido", "doudo", "couto", 
+    "mouro", "ouros", "pouco", "rouco", "bolou", "colou", "mouco", "copas", "colar", "molar", 
+    "polar", "solar", "rolar", "topar", "boiar", "noiar", "moiar", "goiar", "mover", "covil",
     "fungo", "longo", "bingo", "cinto", "lindo", "findo", "vindo", "tinto", "minto", "sinto",
     "pinho", "vinho", "minho", "linho", "ninho", "pinha", "vinha", "tinha", "linha", "minha",
-    "sobra", "cobra", "dobra", "obra", "nobre", "pobre", "sobre", "corte", "morte", "forte",
+    "sobra", "cobra", "dobra", "nobre", "pobre", "sobre", "corte", "morte", "forte",
     "porte", "ponta", "conta", "fonta", "monta", "ronda", "sonda", "banda", "manda", "canda",
     "venda", "renda", "senda", "tenda", "fenda", "lenda", "penda", "pisar", "visar", "lisar",
     "focar", "tocar", "locar", "jogar", "logar", "rogar", "vogar", "cegar", "pegar", "negar",
     "regar", "segar", "legar", "fervo", "servo", "verbo", "berro", "serro", "terra", "serra",
     "berra", "ferra", "perra", "mirra", "birra", "circo", "pinto", "cinto", "finto", "vento",
-    "tento", "lento", "rento", "sento", "mento", "certo", "perto", "ferto", "aberta", "dente",
-    "pente", "lente", "gente", "mente", "tente", "quente", "fonte", "ponte", "monte", "cante",
+    "tento", "lento", "rento", "sento", "mento", "certo", "perto", "ferto", "dente",
+    "pente", "lente", "gente", "mente", "tente", "fonte", "ponte", "monte", "cante",
     "mante", "sante", "tante", "pisam", "visam", "lisam", "risam", "sugam", "fugam", "jugam",
     "puxam", "luxam", "fixam", "mixam", "boxam", "coxam", "roxam", "pixam", "taxam", "faxam"
 ]
 
-
-
-
-palavra = '*****'
+palavra = choice(palavras).upper()
 letras_jogadas =  ''
 incognita = len(palavra)*'_'
 
@@ -97,24 +94,24 @@ while incognita != palavra and chances != 0:
     else:
 
         save = palavras[:]
-        print(save)
 
-        for i in palavras[:]:
-            if letra.lower() in i:
-                palavras.remove(i)
+        if incognita == len(palavra)*'_':
 
-        if len(palavras) == 0:
-            palavras = save[:]
+            for i in palavras[:]:
+                if letra.lower() in i:
+                    palavras.remove(i)
+            
+            if len(palavras) == 0:
+                palavras = save[:]
 
-        palavra = choice(palavras).upper()
+            palavra = choice(palavras).upper()       
 
-
+        print(palavras)
         print(palavra)
 
         incognita = ''
 
         # ESTRUTURA O STICK MAN DE ACORDO COM OS ERROS
-
 
         if letra not in palavra and letra not in letras_jogadas:
             chances -= 1
@@ -151,6 +148,19 @@ while incognita != palavra and chances != 0:
                 incognita += palavra[i]
             else:
                 incognita += '_'
+
+        if incognita != len(palavra)*'_':
+
+            for p in palavras[:]:
+                comp = ''
+                for i in range(len(p)):
+                    if incognita[i] == p[i].upper():
+                        comp += p[i].upper()
+                    else:
+                        comp += '_'
+                if comp != incognita:
+                    palavras.remove(p)
+                print(f'compração: {comp}, {p}')
 
 print(stick_man + '   ' + incognita)
 

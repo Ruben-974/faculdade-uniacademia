30e5f0b0ff7a5d80c6c497a9791f1d1c2ce90672
Ruben-974 <87450219+Ruben-974@users.noreply.github.com>
2026-06-12 10:07:49 -0300

    Update velha.py

--- algoritmos/desafios/velha.py
+++ algoritmos/desafios/velha.py
@@ -1,5 +1,5 @@
-def visualizar_tabulheiro(matriz):
-    tabulheiro = ''
+def montar_tabulheiro(matriz):
+    tabulheiro = '---------\n'
     for i in range(len(matriz)):
         linha = ''
         for j in range(len(matriz)):
@@ -7,36 +7,86 @@ def visualizar_tabulheiro(matriz):
             if j != 2:
                 linha += ' | '
         tabulheiro += linha+'\n'
-    return tabulheiro
+    return tabulheiro + '---------'
+
+
+def validar_jogada(posicao, matriz):
+
+    cont = 0
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if matriz[i][j] in 'xo' and int(posicao) == cont:
+
+                return 'Juiz: "Posição já jogada!"'
+            
+            cont += 1
+    return True
 
 
 def fazer_jogada(posicao, matriz, simbolo):
-    if autorizar_jogada(matriz, simbolo):
+
+    juiz = validar_jogada(posicao, matriz)
+
+    if juiz == True:
+
         for i in range(len(matriz)):
             for j in range(len(matriz)):
                 if matriz[i][j] == posicao:
                     matriz[i][j] = simbolo
-                    return matriz, visualizar_tabulheiro(matriz)
+                    return matriz, juiz
+                
+    return matriz, juiz
 
 
-def autorizar_jogada(matriz, simbolo):
-    if simbolo == 'x' or simbolo == 'o':
-        for i in range(len(matriz)):
-            for j in range(len(matriz)):
-                if matriz[i][j] != 'x' or matriz[i][j] != 'o':
-                    return True
-                else:
-                    return 'Posição já jogada'
-    else:
-        return 'Jogue apenas x ou o'
+def verificar_vitoria_CPU(matriz):
 
+    posicao = None
+
+    
+
+
+    return posicao
+
+
+continuar = True
+msg_juiz = ''
 
 matriz_tabulheiro = [['0', '1', '2'],
-                    ['3', '4', '5'],
-                    ['6', '8', '9']]
+                     ['3', '4', '5'],
+                     ['6', '7', '8']]
 
+print('VAMOS JOGAR O JOGO DA #!')
+print('\nEscolha seu simbolo: x/o')
 
+simbolo_jogador = str(input('Jogador: ')).lower()
 
+while simbolo_jogador not in 'xo' or len(simbolo_jogador) != 1:
 
-matriz_tabulheiro, matriz_tabulheiro_visual = fazer_jogada('0', matriz_tabulheiro, 'y')
-print(matriz_tabulheiro_visual)
+    print('Juiz: "Jogue apenas x ou o!"')
+    print('\nEscolha seu simbolo: x/o')
+
+    simbolo_jogador = str(input('Jogador: ')).lower()
+
+simbolo_CPU = 'o'
+if simbolo_jogador == simbolo_CPU:
+    simbolo_CPU = 'x'
+
+print(f'\nSimbolo Jogador: {simbolo_jogador}')
+print(f'Simbolo CPU: {simbolo_CPU}')
+print(f'\nVAMOS COMEÇAR!\n')
+
+while continuar:
+
+    print(montar_tabulheiro(matriz_tabulheiro))
+
+    print('\nEscolha sua posição: ')
+    jogada_jogador = str(input('Jogador: '))
+
+    if jogada_jogador not in '012345678' or len(jogada_jogador) != 1:
+        print('Juiz: "Escolha uma posição valida!"')
+    else:
+        matriz_tabulheiro, msg_juiz = fazer_jogada(jogada_jogador, matriz_tabulheiro, simbolo_jogador)
+        if msg_juiz != True:
+            print(msg_juiz)
+        else:
+
